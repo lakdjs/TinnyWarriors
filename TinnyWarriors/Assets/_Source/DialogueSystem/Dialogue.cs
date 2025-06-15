94f3a266c981c188926861992b4df1837cf5a2d4
lakdjs <qwerty18112005@gmail.com>
2025-06-15 21:53:53 +0300

    finished dialogue system

--- TinnyWarriors/Assets/_Source/DialogueSystem/Dialogue.cs
+++ TinnyWarriors/Assets/_Source/DialogueSystem/Dialogue.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private string[] lines;
+    [SerializeField] private float textSpeed;
+    private int index;
         
+    // Start is called before the first frame update
+    void Awake()
+    {
+        dialogueText.text = string.Empty;
+        StartDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(dialogueText.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = lines[index];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
