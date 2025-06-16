e37504332c6e1c40b611a1a66970bb61fad920f7
lakdjs <qwerty18112005@gmail.com>
2025-06-16 06:21:05 +0300

    added sceneLoader animation

--- TinnyWarriors/Assets/_Source/EducationSystem/SceneChanger.cs
+++ TinnyWarriors/Assets/_Source/EducationSystem/SceneChanger.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime;
     public void ChangeScene(int sceneID)
     {
-        SceneManager.LoadScene(sceneID);
+        StartCoroutine(LoadScene(sceneID));
+    }
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneIndex);
     }
 }
